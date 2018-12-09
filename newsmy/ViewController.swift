a7716f3535ddac093adc3e1d0deaed021e494b0f
hd <ha_do@yahoo.com>
2018-12-08 17:13:05 -0800

    Initial Commit

---
+++ newsmy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsmy
+//
+//  Created by ha tuong do on 12/8/18.
+//  Copyright © 2018 alpine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
