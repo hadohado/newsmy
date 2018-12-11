59352b6b9f0bc3429612619b77e767fd0d05d501
hd <ha_do@yahoo.com>
2018-12-11 00:27:47 -0800

    3 18 can put newsapi.org info into articles

--- newsmy/NewsHelper.swift
+++ newsmy/NewsHelper.swift
@@ -16,8 +16,41 @@ class NewsHelper {
         
         Alamofire.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=d5a7abc05da045acb54266d081d6a983").responseJSON { ( response) in
             print(response)
+            if let json = response.result.value as? [String: Any] {
+                if let jsonArticles = json["articles"] as? [[String:Any]] {
+                    var articles = [Article]()
+                    
+                    for jsonArticle in jsonArticles {
+                        guard let title = jsonArticle["title"] as? String,
+                            let urlToImage = jsonArticle["urlToImage"] as? String,
+                            let url = jsonArticle["url"] as? String,
+                            let description = jsonArticle["description"] as? String
+                        else {
+                            continue
+                        }
+                        let article = Article()
+                        article.title = title
+                        article.urlToImage = urlToImage
+                        article.url = url
+                     // article.description = description
+                        article.description = description
+                        articles.append(article)
+                        
+                    }
+                }
+            }
         }
         // Alamofire.Request(underlyingQueue: <#DispatchQueue#>)
+        // JSON data is a large dictionary   { articles = { article objects }   }
+        //                                     key      =  value
         
     }
+}
+
+class Article {
+    var title = ""
+    var urlToImage = ""
+    var url = ""
+    var description = ""
+    var category = ""
 }
